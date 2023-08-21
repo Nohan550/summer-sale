5c3e7b1910595c0c53a90dddc553672d18560717
Nohan550 <nohanmuttakim.bd@gmail.com>
2023-08-21 14:11:41 +0600

    second commit

---
+++ javascript/bill.js
@@ -0,0 +1,5 @@
+ function list(id){
+        //    console.log(parseFloat(id.childNodes[3].childNodes[5].childNodes[0].innerText))
+        //  console.log(id.childNodes[3].childNodes[3].innerText)
+        }
+ 
